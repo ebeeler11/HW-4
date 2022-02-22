66cea107018f812114f9a884ac96b2d1f3ee596a
ebeeler11 <98054229+ebeeler11@users.noreply.github.com>
2022-02-21 21:22:17 -0500

    Add files via upload

---
+++ Form1.Designer.cs
@@ -0,0 +1,226 @@
+﻿namespace HSV_Assignment
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.RawPictureBox = new System.Windows.Forms.PictureBox();
+            this.BinaryPictureBox = new System.Windows.Forms.PictureBox();
+            this.HpictureBox = new System.Windows.Forms.PictureBox();
+            this.SpictureBox = new System.Windows.Forms.PictureBox();
+            this.VpictureBox = new System.Windows.Forms.PictureBox();
+            this.RedPictureBox = new System.Windows.Forms.PictureBox();
+            this.BinaryTrackBar = new System.Windows.Forms.TrackBar();
+            this.HminBar = new System.Windows.Forms.TrackBar();
+            this.HmaxBar = new System.Windows.Forms.TrackBar();
+            this.SminBar = new System.Windows.Forms.TrackBar();
+            this.SmaxBar = new System.Windows.Forms.TrackBar();
+            this.VminBar = new System.Windows.Forms.TrackBar();
+            this.VmaxBar = new System.Windows.Forms.TrackBar();
+            ((System.ComponentModel.ISupportInitialize)(this.RawPictureBox)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.BinaryPictureBox)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.HpictureBox)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.SpictureBox)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.VpictureBox)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.RedPictureBox)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.BinaryTrackBar)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.HminBar)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.HmaxBar)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.SminBar)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.SmaxBar)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.VminBar)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.VmaxBar)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // RawPictureBox
+            // 
+            this.RawPictureBox.Location = new System.Drawing.Point(27, 29);
+            this.RawPictureBox.Name = "RawPictureBox";
+            this.RawPictureBox.Size = new System.Drawing.Size(326, 182);
+            this.RawPictureBox.TabIndex = 0;
+            this.RawPictureBox.TabStop = false;
+            // 
+            // BinaryPictureBox
+            // 
+            this.BinaryPictureBox.Location = new System.Drawing.Point(359, 29);
+            this.BinaryPictureBox.Name = "BinaryPictureBox";
+            this.BinaryPictureBox.Size = new System.Drawing.Size(326, 182);
+            this.BinaryPictureBox.TabIndex = 1;
+            this.BinaryPictureBox.TabStop = false;
+            // 
+            // HpictureBox
+            // 
+            this.HpictureBox.Location = new System.Drawing.Point(691, 29);
+            this.HpictureBox.Name = "HpictureBox";
+            this.HpictureBox.Size = new System.Drawing.Size(326, 182);
+            this.HpictureBox.TabIndex = 2;
+            this.HpictureBox.TabStop = false;
+            // 
+            // SpictureBox
+            // 
+            this.SpictureBox.Location = new System.Drawing.Point(1023, 29);
+            this.SpictureBox.Name = "SpictureBox";
+            this.SpictureBox.Size = new System.Drawing.Size(326, 182);
+            this.SpictureBox.TabIndex = 3;
+            this.SpictureBox.TabStop = false;
+            // 
+            // VpictureBox
+            // 
+            this.VpictureBox.Location = new System.Drawing.Point(27, 246);
+            this.VpictureBox.Name = "VpictureBox";
+            this.VpictureBox.Size = new System.Drawing.Size(326, 182);
+            this.VpictureBox.TabIndex = 4;
+            this.VpictureBox.TabStop = false;
+            // 
+            // RedPictureBox
+            // 
+            this.RedPictureBox.Location = new System.Drawing.Point(913, 414);
+            this.RedPictureBox.Name = "RedPictureBox";
+            this.RedPictureBox.Size = new System.Drawing.Size(326, 182);
+            this.RedPictureBox.TabIndex = 5;
+            this.RedPictureBox.TabStop = false;
+            // 
+            // BinaryTrackBar
+            // 
+            this.BinaryTrackBar.Location = new System.Drawing.Point(380, 246);
+            this.BinaryTrackBar.Maximum = 255;
+            this.BinaryTrackBar.Name = "BinaryTrackBar";
+            this.BinaryTrackBar.Size = new System.Drawing.Size(305, 56);
+            this.BinaryTrackBar.TabIndex = 6;
+            this.BinaryTrackBar.Scroll += new System.EventHandler(this.BinaryTrackBar_Scroll);
+            // 
+            // HminBar
+            // 
+            this.HminBar.Location = new System.Drawing.Point(712, 246);
+            this.HminBar.Maximum = 255;
+            this.HminBar.Name = "HminBar";
+            this.HminBar.Size = new System.Drawing.Size(305, 56);
+            this.HminBar.TabIndex = 7;
+            this.HminBar.Scroll += new System.EventHandler(this.HminBar_Scroll);
+            // 
+            // HmaxBar
+            // 
+            this.HmaxBar.Location = new System.Drawing.Point(712, 330);
+            this.HmaxBar.Maximum = 255;
+            this.HmaxBar.Name = "HmaxBar";
+            this.HmaxBar.Size = new System.Drawing.Size(305, 56);
+            this.HmaxBar.TabIndex = 8;
+            this.HmaxBar.Scroll += new System.EventHandler(this.HmaxBar_Scroll);
+            // 
+            // SminBar
+            // 
+            this.SminBar.Location = new System.Drawing.Point(1044, 246);
+            this.SminBar.Maximum = 255;
+            this.SminBar.Name = "SminBar";
+            this.SminBar.Size = new System.Drawing.Size(305, 56);
+            this.SminBar.TabIndex = 9;
+            this.SminBar.Scroll += new System.EventHandler(this.SminBar_Scroll);
+            // 
+            // SmaxBar
+            // 
+            this.SmaxBar.Location = new System.Drawing.Point(1044, 330);
+            this.SmaxBar.Maximum = 255;
+            this.SmaxBar.Name = "SmaxBar";
+            this.SmaxBar.Size = new System.Drawing.Size(305, 56);
+            this.SmaxBar.TabIndex = 10;
+            this.SmaxBar.Scroll += new System.EventHandler(this.SmaxBar_Scroll);
+            // 
+            // VminBar
+            // 
+            this.VminBar.Location = new System.Drawing.Point(48, 464);
+            this.VminBar.Maximum = 255;
+            this.VminBar.Name = "VminBar";
+            this.VminBar.Size = new System.Drawing.Size(305, 56);
+            this.VminBar.TabIndex = 11;
+            this.VminBar.Scroll += new System.EventHandler(this.VminBar_Scroll);
+            // 
+            // VmaxBar
+            // 
+            this.VmaxBar.Location = new System.Drawing.Point(48, 526);
+            this.VmaxBar.Maximum = 255;
+            this.VmaxBar.Name = "VmaxBar";
+            this.VmaxBar.Size = new System.Drawing.Size(305, 56);
+            this.VmaxBar.TabIndex = 12;
+            this.VmaxBar.Scroll += new System.EventHandler(this.VmaxBar_Scroll);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(1479, 629);
+            this.Controls.Add(this.VmaxBar);
+            this.Controls.Add(this.VminBar);
+            this.Controls.Add(this.SmaxBar);
+            this.Controls.Add(this.SminBar);
+            this.Controls.Add(this.HmaxBar);
+            this.Controls.Add(this.HminBar);
+            this.Controls.Add(this.BinaryTrackBar);
+            this.Controls.Add(this.RedPictureBox);
+            this.Controls.Add(this.VpictureBox);
+            this.Controls.Add(this.SpictureBox);
+            this.Controls.Add(this.HpictureBox);
+            this.Controls.Add(this.BinaryPictureBox);
+            this.Controls.Add(this.RawPictureBox);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.Load += new System.EventHandler(this.Form1_Load_1);
+            ((System.ComponentModel.ISupportInitialize)(this.RawPictureBox)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.BinaryPictureBox)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.HpictureBox)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.SpictureBox)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.VpictureBox)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.RedPictureBox)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.BinaryTrackBar)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.HminBar)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.HmaxBar)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.SminBar)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.SmaxBar)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.VminBar)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.VmaxBar)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.PictureBox RawPictureBox;
+        private System.Windows.Forms.PictureBox BinaryPictureBox;
+        private System.Windows.Forms.PictureBox HpictureBox;
+        private System.Windows.Forms.PictureBox SpictureBox;
+        private System.Windows.Forms.PictureBox VpictureBox;
+        private System.Windows.Forms.PictureBox RedPictureBox;
+        private System.Windows.Forms.TrackBar BinaryTrackBar;
+        private System.Windows.Forms.TrackBar HminBar;
+        private System.Windows.Forms.TrackBar HmaxBar;
+        private System.Windows.Forms.TrackBar SminBar;
+        private System.Windows.Forms.TrackBar SmaxBar;
+        private System.Windows.Forms.TrackBar VminBar;
+        private System.Windows.Forms.TrackBar VmaxBar;
+    }
+}
+
---
+++ Form1.cs
@@ -0,0 +1,143 @@
+﻿using Emgu.CV;
+using Emgu.CV.Structure;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace HSV_Assignment
+    //Eric Beeler
+{
+    public partial class Form1 : Form
+    {
+        //initialize bars
+        private int _HmaxBar = 33;
+        private int _HminBar = 17;
+        private int _SmaxBar = 255;
+        private int _SminBar = 93;
+        private int _VmaxBar = 255;
+        private int _VminBar = 151;
+        private int _BinaryTrackBar = 150;
+
+        private VideoCapture _capture;
+        private Thread _captureThread;
+
+
+
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void Form1_Load_1(object sender, EventArgs e)
+        {
+            //setup values and webcam
+            _capture = new VideoCapture(0);
+            _captureThread = new Thread(DisplayWebcam);
+            _captureThread.Start();
+            BinaryTrackBar.Value = _BinaryTrackBar;
+            HminBar.Value = _HminBar;
+            HmaxBar.Value = _HmaxBar;
+            SminBar.Value = _SminBar;
+            SmaxBar.Value = _SmaxBar;
+            VminBar.Value = _VminBar;
+            HmaxBar.Value = _VmaxBar;
+
+   
+        }
+       
+
+        private void DisplayWebcam()
+        {
+            while (_capture.IsOpened)
+            {
+                Mat frame = _capture.QueryFrame();
+                // create new box size
+                int newHeight = (frame.Size.Height * RawPictureBox.Size.Width) / frame.Size.Width;
+                Size newSize = new Size(RawPictureBox.Size.Width, newHeight);
+                CvInvoke.Resize(frame, frame, newSize);
+
+                RawPictureBox.Image = frame.Bitmap;
+                // convert to and display binary box
+                Mat grayFrame = new Mat();
+                Mat binaryFrame = new Mat();
+                CvInvoke.CvtColor(frame, grayFrame, Emgu.CV.CvEnum.ColorConversion.Bgr2Gray);
+                CvInvoke.Threshold(grayFrame, binaryFrame, _BinaryTrackBar, 255, Emgu.CV.CvEnum.ThresholdType.Binary);
+               
+                BinaryPictureBox.Image = binaryFrame.Bitmap;
+
+                Mat hsvFrame = new Mat();
+                CvInvoke.CvtColor(frame, hsvFrame, Emgu.CV.CvEnum.ColorConversion.Bgr2Hsv);
+                //setup hsv
+                Mat[] hsvChannels = hsvFrame.Split();
+                //complete h,s,and v channels and their respective boxes
+                Mat hueFilter = new Mat();
+                CvInvoke.InRange(hsvChannels[0], new ScalarArray(_HminBar), new ScalarArray(_HmaxBar), hueFilter);
+                Invoke(new Action(() => { HpictureBox.Image = hueFilter.Bitmap; }));
+
+                Mat saturationFilter = new Mat();
+                CvInvoke.InRange(hsvChannels[1], new ScalarArray(_SminBar), new ScalarArray(_SmaxBar), saturationFilter);
+                Invoke(new Action(() => { SpictureBox.Image = saturationFilter.Bitmap; }));
+
+                Mat valueFilter = new Mat();
+                CvInvoke.InRange(hsvChannels[2], new ScalarArray(_VminBar), new ScalarArray(_VmaxBar), valueFilter);
+                Invoke(new Action(() => { VpictureBox.Image = valueFilter.Bitmap; }));
+                //assemble into combined image
+                Mat combinedImage = new Mat();
+                CvInvoke.BitwiseAnd(hueFilter, saturationFilter, combinedImage);
+                CvInvoke.BitwiseAnd(combinedImage, valueFilter, combinedImage);
+                Invoke(new Action(() => { RedPictureBox.Image = combinedImage.Bitmap; }));
+            }
+        }
+
+        private void Form1_FormClosing(object sender, FormClosedEventArgs e)
+        {
+            _captureThread.Abort();
+        }
+
+        //Set up scroll bars
+        private void BinaryTrackBar_Scroll(object sender, EventArgs e)
+        {
+            _BinaryTrackBar = BinaryTrackBar.Value;
+        }
+
+        private void HminBar_Scroll(object sender, EventArgs e)
+        {
+            _HminBar = HminBar.Value;
+        }
+
+        private void HmaxBar_Scroll(object sender, EventArgs e)
+        {
+            _HmaxBar = HmaxBar.Value;
+        }
+
+        private void SminBar_Scroll(object sender, EventArgs e)
+        {
+            _SminBar = SminBar.Value;
+        }
+
+        private void SmaxBar_Scroll(object sender, EventArgs e)
+        {
+            _SmaxBar = SmaxBar.Value;
+
+        }
+
+        private void VminBar_Scroll(object sender, EventArgs e)
+        {
+            _VminBar = VminBar.Value;
+        }
+
+        private void VmaxBar_Scroll(object sender, EventArgs e)
+        {
+            _VmaxBar = VmaxBar.Value;
+        }
+
+     
+    }       
+}
